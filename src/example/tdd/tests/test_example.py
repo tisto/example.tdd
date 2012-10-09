63495ebf0c7980cad285b9bc5c5ce0837e39c70f
Timo Stollenwerk <contact@timostollenwerk.net>
2012-10-09 13:01:03 +0200

    Example integration test updated.

--- src/example/tdd/tests/test_example.py
+++ src/example/tdd/tests/test_example.py
@@ -1,25 +1,21 @@
 import unittest2 as unittest
 
-from Products.CMFCore.utils import getToolByName
-
 from example.tdd.testing import \
     EXAMPLE_TDD_INTEGRATION_TESTING
 
 
-class TestExample(unittest.TestCase):
+class ExampleIntegrationTest(unittest.TestCase):
 
     layer = EXAMPLE_TDD_INTEGRATION_TESTING
 
     def setUp(self):
-        self.app = self.layer['app']
-        self.portal = self.layer['portal']
-        self.qi_tool = getToolByName(self.portal, 'portal_quickinstaller')
+        pass
 
-    def test_product_is_installed(self):
-        """ Validate that our products GS profile has been run and the product
-            installed
-        """
-        pid = 'example.tdd'
-        installed = [p['id'] for p in self.qi_tool.listInstalledProducts()]
-        self.assertTrue(pid in installed,
-                        'package appears not to have been installed')
+    def test_success(self):
+        sum = 1 + 3
+        self.assertEqual(sum, 4)
+
+
+
+
+
